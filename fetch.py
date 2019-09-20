4211be9d554e9d75ee3c015209083f1cad7edefe
Boidushya <boidushyabhattacharya@gmail.com>
2019-09-20 15:48:18 +0530

    Update fetch.py

--- fetch.py
+++ fetch.py
@@ -3,6 +3,16 @@ import urllib.request
 from random import choice as rc
 import sys
 import os
+import requests
+
+def reqImg():
+    r= requests.get("https://www.shitpostbot.com/api/randsource")
+    img_url = "https://www.shitpostbot.com/"
+    content = str(r.content)[2:-1:]
+    content = content.replace('null', '0')
+    img_url += eval(content)['sub']['img']['full'].replace('\\', '')
+    return img_url
+
 def get_lnk(query):
     url = "https://www.shitpostbot.com/gallery/sourceimages?review_state=accepted&query=" + query +"&order=total_rating&direction=DESC"
     response = urllib.request.urlopen(url)
@@ -10,8 +20,11 @@ def get_lnk(query):
     result = []
     for div in soup.findAll('div', attrs={'class':'img'}):
         result.append(div.find('a')['href'])
-    fin = rc(result)
-    return 'https://www.shitpostbot.com' + fin
+    if len(result) == 0:
+        raise Exception('Sorry, couldn\'t find anything :(')
+    else:
+        fin = rc(result)
+        return 'https://www.shitpostbot.com' + fin
 
 def get_img(cmd):
     url = get_lnk(cmd)
@@ -27,13 +40,16 @@ def dl(url):
     urllib.request.urlretrieve(url,'images/' + url.replace('https://www.shitpostbot.com/img/sourceimages/', ''))
     print('Successfully downloaded ' + url.replace('https://www.shitpostbot.com/img/sourceimages/', ''))
 
-# if __name__ == '__main__':
-#     url = get_img('nice')
-#     dl(url)
-
 if len(sys.argv) <= 2 :
     print("\nUsage:\npython fetch.py -q [query] \n ")
     sys.exit (1)
-elif sys.argv[1] == '-q':
-    url = get_img(str(sys.argv[2]))
-    dl(url)
+elif '-q' in sys.argv:
+    index = sys.argv.index('-q')
+    if sys.argv[index + 1] == '--rand':
+        print('Downloading Random Image from ShitPostBot...')
+        image = reqImg()
+        dl(image)
+    else:
+        url = get_img(str(sys.argv[index + 1]))
+        print('Downloading %s from ShitPostBot...'%(str(sys.argv[index + 1])))
+        dl(url)
